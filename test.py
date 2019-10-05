4473946c4d712a46126e167535b487f35b09eaae
Raja Raman <raja.raman@kwikee.com>
2019-10-05 11:25:23 -0400

    AWS cli updated 19

---
+++ test.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# the above line is to avoid 'SyntaxError: Non-UTF-8 code starting with' error
+
+'''
+Created on 
+Course work: 
+@author: raja
+Source:
+    
+'''
+
+# Import necessary modules
+
+
+def startpy():
+    print('Hey there')
+
+
+if __name__ == '__main__':
+    startpy()
